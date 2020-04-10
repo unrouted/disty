7fd5a8fcffe412faa6796f6314f1b878c33d4e95
John Carr <john.carr@unrouted.co.uk>
2020-04-10 16:51:37 +0100

    Add missing file

---
+++ distribd/jobs.py
@@ -0,0 +1,35 @@
+import asyncio
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class WorkerPool:
+    def __init__(self):
+        self._jobs = set()
+
+    @property
+    def active(self):
+        return len(self._jobs)
+
+    def spawn(self, coro):
+        task = asyncio.ensure_future(coro)
+
+        def done_callback(future):
+            self._jobs.discard(task)
+
+            try:
+                future.result()
+            except asyncio.CancelledError:
+                pass
+            except Exception:
+                logger.exception("Unhandled exception in async task")
+
+        task.add_done_callback(done_callback)
+
+        self._jobs.add(task)
+        return task
+
+    async def close(self):
+        [task.cancel() for task in self._jobs]
+        await asyncio.gather(*[task for task in self._jobs])
