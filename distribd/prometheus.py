f4d500aa421aa49d080e1b79cc40c2e9c49fef59
John Carr <john.carr@unrouted.co.uk>
2020-04-14 17:41:01 +0100

    Make prometheus endpoint return something

--- distribd/prometheus.py
+++ distribd/prometheus.py
@@ -1,18 +1,21 @@
 from aiohttp import web
+from prometheus_client import CONTENT_TYPE_LATEST, generate_latest
 
 from .utils.web import run_server
 
 routes = web.RouteTableDef()
 
 
-@routes.get("/ok")
+@routes.get("/healthz")
 async def ok(request):
     return web.json_response({"ok": True})
 
 
 @routes.get("/metrics")
 async def metrics(request):
-    return web.json_response({"ok": True})
+    return web.Response(
+        body=generate_latest(), headers={"Content-Type": CONTENT_TYPE_LATEST}
+    )
 
 
 async def run_prometheus(identifier, registry_state, images_directory, port):
