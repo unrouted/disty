766e86f3b2dce3d9dae61b02364548ac861fad0a
John Carr <john.carr@unrouted.co.uk>
2020-05-04 17:13:37 +0100

    More cleanup

--- distribd/state.py
+++ distribd/state.py
@@ -11,6 +11,13 @@ ATTR_SIZE = "size"
 ATTR_DEPENDENCIES = "dependencies"
 ATTR_HASH = "hash"
 ATTR_REPOSITORY = "repository"
+ATTR_TAG = "tag"
+ATTR_REPOSITORIES = "repositories"
+ATTR_TYPE = "type"
+
+TYPE_MANIFEST = "manifest"
+TYPE_BLOB = "blob"
+TYPE_TAG = "tag"
 
 
 class Reducer:
@@ -38,7 +45,12 @@ class RegistryState(Reducer):
         if hash not in self.graph.nodes:
             return False
 
-        if repository not in self.graph.nodes[hash]["repositories"]:
+        blob = self.graph.nodes[hash]
+
+        if blob[ATTR_TYPE] != TYPE_BLOB:
+            return False
+
+        if repository not in self.graph.nodes[hash][ATTR_REPOSITORIES]:
             return False
 
         return True
@@ -49,10 +61,13 @@ class RegistryState(Reducer):
 
         manifest = self.graph.nodes[hash]
 
-        if "content_type" not in manifest:
+        if manifest[ATTR_TYPE] != TYPE_MANIFEST:
             return False
 
-        if repository not in self.graph.nodes[hash]["repositories"]:
+        if ATTR_CONTENT_TYPE not in manifest:
+            return False
+
+        if repository not in self.graph.nodes[hash][ATTR_REPOSITORIES]:
             return False
 
         return True
@@ -68,52 +83,60 @@ class RegistryState(Reducer):
         logger.critical("Applying %s", entry)
 
         if entry["type"] == RegistryActions.HASH_TAGGED:
-            repository = self.state.setdefault(entry["repository"], {})
-            repository[entry["tag"]] = entry["hash"]
+            repository = self.state.setdefault(entry[ATTR_REPOSITORY], {})
+            repository[entry[ATTR_TAG]] = entry[ATTR_HASH]
 
-            tags_for_hash = self.tags_for_hash.setdefault(entry["hash"], set())
-            tags_for_hash.add((entry["repository"], entry["tag"]))
+            tags_for_hash = self.tags_for_hash.setdefault(entry[ATTR_HASH], set())
+            tags_for_hash.add((entry[ATTR_REPOSITORY], entry[ATTR_TAG]))
 
         elif entry["type"] == RegistryActions.BLOB_MOUNTED:
-            if entry["hash"] not in self.graph.nodes:
-                self.graph.add_node(entry["hash"])
+            if entry[ATTR_HASH] not in self.graph.nodes:
+                self.graph.add_node(entry[ATTR_HASH], **{ATTR_TYPE: TYPE_BLOB})
 
-            self.graph.nodes[entry["hash"]].setdefault("repositories", set()).add(
-                entry["repository"]
+            self.graph.nodes[entry[ATTR_HASH]].setdefault(ATTR_REPOSITORIES, set()).add(
+                entry[ATTR_REPOSITORY]
             )
 
         elif entry["type"] == RegistryActions.BLOB_UNMOUNTED:
-            self.graph.nodes[entry["hash"]]["repositories"].discard(entry["repository"])
+            self.graph.nodes[entry[ATTR_HASH]][ATTR_REPOSITORIES].discard(
+                entry[ATTR_REPOSITORY]
+            )
 
         elif entry["type"] == RegistryActions.BLOB_INFO:
-            for dependency in entry["dependencies"]:
-                self.graph.add_edge(entry["hash"], dependency)
-            self.graph.nodes[entry["hash"]]["content_type"] = entry["content_type"]
+            for dependency in entry[ATTR_DEPENDENCIES]:
+                self.graph.add_edge(entry[ATTR_HASH], dependency)
+            self.graph.nodes[entry[ATTR_HASH]][ATTR_CONTENT_TYPE] = entry[
+                ATTR_CONTENT_TYPE
+            ]
 
         elif entry["type"] == RegistryActions.BLOB_STAT:
-            self.graph.nodes[entry["hash"]]["size"] = entry["size"]
+            self.graph.nodes[entry[ATTR_HASH]][ATTR_SIZE] = entry[ATTR_SIZE]
 
         elif entry["type"] == RegistryActions.MANIFEST_MOUNTED:
-            if entry["hash"] not in self.graph.nodes:
-                self.graph.add_node(entry["hash"])
+            if entry[ATTR_HASH] not in self.graph.nodes:
+                self.graph.add_node(entry[ATTR_HASH], **{ATTR_TYPE: TYPE_MANIFEST})
 
-            self.graph.nodes[entry["hash"]].setdefault("repositories", set()).add(
-                entry["repository"]
+            self.graph.nodes[entry[ATTR_HASH]].setdefault(ATTR_REPOSITORIES, set()).add(
+                entry[ATTR_REPOSITORY]
             )
 
         elif entry["type"] == RegistryActions.MANIFEST_UNMOUNTED:
-            self.graph.nodes[entry["hash"]]["repositories"].discard(entry["repository"])
+            self.graph.nodes[entry[ATTR_HASH]][ATTR_REPOSITORIES].discard(
+                entry[ATTR_REPOSITORY]
+            )
 
             # Legacy
-            for repository, tag in set(self.tags_for_hash.get(entry["hash"], [])):
-                if repository != entry["repository"]:
+            for repository, tag in set(self.tags_for_hash.get(entry[ATTR_HASH], [])):
+                if repository != entry[ATTR_REPOSITORY]:
                     continue
                 self.state.get(repository, {}).pop(tag, None)
 
         elif entry["type"] == RegistryActions.MANIFEST_INFO:
-            for dependency in entry["dependencies"]:
-                self.graph.add_edge(entry["hash"], dependency)
-            self.graph.nodes[entry["hash"]]["content_type"] = entry["content_type"]
+            for dependency in entry[ATTR_DEPENDENCIES]:
+                self.graph.add_edge(entry[ATTR_HASH], dependency)
+            self.graph.nodes[entry[ATTR_HASH]][ATTR_CONTENT_TYPE] = entry[
+                ATTR_CONTENT_TYPE
+            ]
 
         elif entry["type"] == RegistryActions.MANIFEST_INFO:
-            self.graph.nodes[entry["hash"]]["size"] = entry["size"]
+            self.graph.nodes[entry[ATTR_HASH]][ATTR_SIZE] = entry[ATTR_SIZE]
