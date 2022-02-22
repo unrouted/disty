39bba9c10dc0b98b5d79b8c2fa59a31b23f449c4
John Carr <john.carr@unrouted.co.uk>
2022-02-22 16:07:57 +0000

    Blacken tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,6 +11,7 @@ import pytest
 def fixtures_path():
     return pathlib.Path(__file__).parent / "fixtures"
 
+
 @pytest.fixture(autouse=True)
 def configure_logging(caplog):
     caplog.set_level(logging.DEBUG)
