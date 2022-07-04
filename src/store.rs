99007da70a486c2a7cf206bfc4b6bf8f01d845ce
John Carr <john.carr@unrouted.co.uk>
2022-07-04 11:09:04 +0100

    Remove test code and comments that don't make sense

--- src/store.rs
+++ src/store.rs
@@ -1,25 +1,3 @@
-//! Represents the storage trait and example implementation.
-//!
-//! The storage trait is used to house and eventually serialize the state of the system.
-//! Custom implementations of this are normal and this is likely to be a key integration
-//! point for your distributed storage.
-
-// Copyright 2019 TiKV Project Authors. Licensed under Apache-2.0.
-
-// Copyright 2015 The etcd Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 use std::cmp;
 use std::sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard};
 
@@ -28,17 +6,10 @@ use raft::{eraftpb::*, RaftState, Storage};
 use raft::util::limit_size;
 use raft::{Error, Result, StorageError};
 
-/// The Memory Storage Core instance holds the actual state of the storage struct. To access this
-/// value, use the `rl` and `wl` functions on the main RegistryStorage implementation.
 pub struct RegistryStorageCore {
     raft_state: RaftState,
-    // entries[i] has raft log position i+snapshot.get_metadata().index
     entries: Vec<Entry>,
-    // Metadata of the last snapshot received.
     snapshot_metadata: SnapshotMetadata,
-    // If it is true, the next snapshot will return a
-    // SnapshotTemporarilyUnavailable error.
-    trigger_snap_unavailable: bool,
 }
 
 impl Default for RegistryStorageCore {
@@ -46,35 +17,24 @@ impl Default for RegistryStorageCore {
         RegistryStorageCore {
             raft_state: Default::default(),
             entries: vec![],
-            // Every time a snapshot is applied to the storage, the metadata will be stored here.
             snapshot_metadata: Default::default(),
-            // When starting from scratch populate the list with a dummy entry at term zero.
-            trigger_snap_unavailable: false,
         }
     }
 }
 
 impl RegistryStorageCore {
-    /// Saves the current HardState.
     pub fn set_hardstate(&mut self, hs: HardState) {
         self.raft_state.hard_state = hs;
     }
 
-    /// Get the hard state.
     pub fn hard_state(&self) -> &HardState {
         &self.raft_state.hard_state
     }
 
-    /// Get the mut hard state.
     pub fn mut_hard_state(&mut self) -> &mut HardState {
         &mut self.raft_state.hard_state
     }
 
-    /// Commit to an index.
-    ///
-    /// # Panics
-    ///
-    /// Panics if there is no such entry in raft logs.
     pub fn commit_to(&mut self, index: u64) -> Result<()> {
         assert!(
             self.has_entry_at(index),
@@ -88,7 +48,6 @@ impl RegistryStorageCore {
         Ok(())
     }
 
-    /// Saves the current conf state.
     pub fn set_conf_state(&mut self, cs: ConfState) {
         self.raft_state.conf_state = cs;
     }
@@ -112,11 +71,6 @@ impl RegistryStorageCore {
         }
     }
 
-    /// Overwrites the contents of this Storage object with those of the given snapshot.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the snapshot index is less than the storage's first index.
     pub fn apply_snapshot(&mut self, mut snapshot: Snapshot) -> Result<()> {
         let mut meta = snapshot.take_metadata();
         let index = meta.index;
@@ -139,9 +93,6 @@ impl RegistryStorageCore {
     fn snapshot(&self) -> Snapshot {
         let mut snapshot = Snapshot::default();
 
-        // We assume all entries whose indexes are less than `hard_state.commit`
-        // have been applied, so use the latest commit index to construct the snapshot.
-        // TODO: This is not true for async ready.
         let meta = snapshot.mut_metadata();
         meta.index = self.raft_state.hard_state.commit;
         meta.term = match meta.index.cmp(&self.snapshot_metadata.index) {
@@ -162,13 +113,6 @@ impl RegistryStorageCore {
         snapshot
     }
 
-    /// Discards all log entries prior to compact_index.
-    /// It is the application's responsibility to not attempt to compact an index
-    /// greater than RaftLog.applied.
-    ///
-    /// # Panics
-    ///
-    /// Panics if `compact_index` is higher than `Storage::last_index(&self) + 1`.
     pub fn compact(&mut self, compact_index: u64) -> Result<()> {
         if compact_index <= self.first_index() {
             // Don't need to treat this case as an error.
@@ -190,12 +134,6 @@ impl RegistryStorageCore {
         Ok(())
     }
 
-    /// Append the new entries to storage.
-    ///
-    /// # Panics
-    ///
-    /// Panics if `ents` contains compacted entries, or there's a gap between `ents` and the last
-    /// received entry in the storage.
     pub fn append(&mut self, ents: &[Entry]) -> Result<()> {
         if ents.is_empty() {
             return Ok(());
@@ -215,14 +153,12 @@ impl RegistryStorageCore {
             );
         }
 
-        // Remove all entries overwritten by `ents`.
         let diff = ents[0].index - self.first_index();
         self.entries.drain(diff as usize..);
         self.entries.extend_from_slice(&ents);
         Ok(())
     }
 
-    /// Commit to `idx` and set configuration to the given states. Only used for tests.
     pub fn commit_to_and_set_conf_states(&mut self, idx: u64, cs: Option<ConfState>) -> Result<()> {
         self.commit_to(idx)?;
         if let Some(cs) = cs {
@@ -230,37 +166,21 @@ impl RegistryStorageCore {
         }
         Ok(())
     }
-
-    /// Trigger a SnapshotTemporarilyUnavailable error.
-    pub fn trigger_snap_unavailable(&mut self) {
-        self.trigger_snap_unavailable = true;
-    }
 }
 
-/// `RegistryStorage` is a thread-safe but incomplete implementation of `Storage`, mainly for tests.
-///
-/// A real `Storage` should save both raft logs and applied data. However `RegistryStorage` only
-/// contains raft logs. So you can call `RegistryStorage::append` to persist new received unstable raft
-/// logs and then access them with `Storage` APIs. The only exception is `Storage::snapshot`. There
-/// is no data in `Snapshot` returned by `RegistryStorage::snapshot` because applied data is not stored
-/// in `RegistryStorage`.
+
 #[derive(Clone, Default)]
 pub struct RegistryStorage {
     core: Arc<RwLock<RegistryStorageCore>>,
 }
 
 impl RegistryStorage {
-    /// Returns a new memory storage value.
     pub fn new() -> RegistryStorage {
         RegistryStorage {
             ..Default::default()
         }
     }
 
-    /// Create a new `RegistryStorage` with a given `Config`. The given `Config` will be used to
-    /// initialize the storage.
-    ///
-    /// You should use the same input to initialize all nodes.
     pub fn new_with_conf_state<T>(conf_state: T) -> RegistryStorage
     where
         ConfState: From<T>,
@@ -270,44 +190,29 @@ impl RegistryStorage {
         store
     }
 
-    /// Initialize a `RegistryStorage` with a given `Config`.
-    ///
-    /// You should use the same input to initialize all nodes.
     pub fn initialize_with_conf_state<T>(&self, conf_state: T)
     where
         ConfState: From<T>,
     {
         assert!(!self.initial_state().unwrap().initialized());
         let mut core = self.wl();
-        // Setting initial state is very important to build a correct raft, as raft algorithm
-        // itself only guarantees logs consistency. Typically, you need to ensure either all start
-        // states are the same on all nodes, or new nodes always catch up logs by snapshot first.
-        //
-        // In practice, we choose the second way by assigning non-zero index to first index. Here
-        // we choose the first way for historical reason and easier to write tests.
         core.raft_state.conf_state = ConfState::from(conf_state);
     }
 
-    /// Opens up a read lock on the storage and returns a guard handle. Use this
-    /// with functions that don't require mutation.
     pub fn rl(&self) -> RwLockReadGuard<'_, RegistryStorageCore> {
         self.core.read().unwrap()
     }
 
-    /// Opens up a write lock on the storage and returns guard handle. Use this
-    /// with functions that take a mutable reference to self.
     pub fn wl(&self) -> RwLockWriteGuard<'_, RegistryStorageCore> {
         self.core.write().unwrap()
     }
 }
 
 impl Storage for RegistryStorage {
-    /// Implements the Storage trait.
     fn initial_state(&self) -> Result<RaftState> {
         Ok(self.rl().raft_state.clone())
     }
 
-    /// Implements the Storage trait.
     fn entries(&self, low: u64, high: u64, max_size: impl Into<Option<u64>>) -> Result<Vec<Entry>> {
         let max_size = max_size.into();
         let core = self.rl();
@@ -331,7 +236,6 @@ impl Storage for RegistryStorage {
         Ok(ents)
     }
 
-    /// Implements the Storage trait.
     fn term(&self, idx: u64) -> Result<u64> {
         let core = self.rl();
         if idx == core.snapshot_metadata.index {
@@ -349,28 +253,20 @@ impl Storage for RegistryStorage {
         Ok(core.entries[(idx - offset) as usize].term)
     }
 
-    /// Implements the Storage trait.
     fn first_index(&self) -> Result<u64> {
         Ok(self.rl().first_index())
     }
 
-    /// Implements the Storage trait.
     fn last_index(&self) -> Result<u64> {
         Ok(self.rl().last_index())
     }
 
-    /// Implements the Storage trait.
     fn snapshot(&self, request_index: u64) -> Result<Snapshot> {
         let mut core = self.wl();
-        if core.trigger_snap_unavailable {
-            core.trigger_snap_unavailable = false;
-            Err(Error::Store(StorageError::SnapshotTemporarilyUnavailable))
-        } else {
-            let mut snap = core.snapshot();
-            if snap.get_metadata().index < request_index {
-                snap.mut_metadata().index = request_index;
-            }
-            Ok(snap)
+        let mut snap = core.snapshot();
+        if snap.get_metadata().index < request_index {
+            snap.mut_metadata().index = request_index;
         }
+        Ok(snap)
     }
 }
