4db27916e1d14077dc5ccbe67d5c4fc801745329
John Carr <john.carr@unrouted.co.uk>
2023-03-13 14:13:00 +0000

    Missing file

---
+++ src/network/registry/errors.rs
@@ -0,0 +1,94 @@
+use actix_web::http::StatusCode;
+use actix_web::{HttpResponse, HttpResponseBuilder, ResponseError};
+
+use crate::network::registry::utils::simple_oci_error;
+use crate::types::{Digest, RepositoryName};
+
+#[derive(Debug)]
+pub(crate) enum RegistryError {
+    ServiceUnavailable {},
+    MustAuthenticate {
+        challenge: String,
+    },
+    AccessDenied {},
+    DigestInvalid {},
+    BlobNotFound {},
+    UploadInvalid {},
+    UploadComplete {
+        repository: RepositoryName,
+        digest: Digest,
+    },
+    UploadedPart {
+        repository: RepositoryName,
+        upload_id: String,
+        size: u64,
+    },
+}
+
+impl std::fmt::Display for RegistryError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        // Customize so only `x` and `y` are denoted.
+        write!(f, "{:?}", self)
+    }
+}
+
+impl ResponseError for RegistryError {
+    fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
+        match self {
+            Self::ServiceUnavailable {} => {
+                HttpResponseBuilder::new(StatusCode::SERVICE_UNAVAILABLE).finish()
+            }
+            Self::MustAuthenticate { challenge } => {
+                let body = simple_oci_error("UNAUTHORIZED", "authentication required");
+
+                HttpResponseBuilder::new(StatusCode::UNAUTHORIZED)
+                    .append_header(("Www-Authenticate", challenge.clone()))
+                    .body(body)
+            }
+            Self::AccessDenied {} => {
+                let body = simple_oci_error("DENIED", "requested access to the resource is denied");
+
+                HttpResponseBuilder::new(StatusCode::FORBIDDEN).body(body)
+            }
+            Self::DigestInvalid {} => {
+                let body = simple_oci_error(
+                    "DIGEST_INVALID",
+                    "provided digest did not match uploaded content",
+                );
+
+                HttpResponseBuilder::new(StatusCode::BAD_REQUEST).body(body)
+            }
+            Self::BlobNotFound {} => {
+                let body = simple_oci_error("BLOB_NOT_FOUND", "blob not found");
+
+                HttpResponseBuilder::new(StatusCode::BAD_REQUEST).body(body)
+            }
+            Self::UploadInvalid {} => {
+                let body = simple_oci_error("BLOB_UPLOAD_INVALID", "the upload was invalid");
+
+                HttpResponseBuilder::new(StatusCode::BAD_REQUEST).body(body)
+            }
+            Self::UploadComplete { repository, digest } => {
+                HttpResponseBuilder::new(StatusCode::CREATED)
+                    .append_header(("Location", format!("/v2/{repository}/blobs/{digest}")))
+                    .append_header(("Range", "0-0"))
+                    .append_header(("Content-Length", "0"))
+                    .append_header(("Docker-Content-Digest", digest.to_string()))
+                    .finish()
+            }
+            Self::UploadedPart {
+                repository,
+                upload_id,
+                size,
+            } => HttpResponseBuilder::new(StatusCode::ACCEPTED)
+                .append_header((
+                    "Location",
+                    format!("/v2/{repository}/blobs/uploads/{upload_id}"),
+                ))
+                .append_header(("Range", format!("0-{size}")))
+                .append_header(("Content-Length", "0"))
+                .append_header(("Docker-Upload-UUID", upload_id.clone()))
+                .finish(),
+        }
+    }
+}
