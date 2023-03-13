477e3921c6aa3d4e7c0ed63a68f4bbf216076e1d
John Carr <john.carr@unrouted.co.uk>
2023-03-13 18:38:10 +0000

    Add blob DELETE

---
+++ src/network/registry/blobs/delete.rs
@@ -0,0 +1,62 @@
+use crate::app::RegistryApp;
+use crate::extractors::Token;
+
+use crate::network::registry::errors::RegistryError;
+use crate::types::Digest;
+use crate::types::RegistryAction;
+use crate::types::RepositoryName;
+use actix_web::delete;
+use actix_web::http::StatusCode;
+use actix_web::web::Data;
+use actix_web::web::Path;
+use actix_web::HttpRequest;
+use actix_web::HttpResponse;
+use actix_web::HttpResponseBuilder;
+use chrono::prelude::*;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct BlobRequest {
+    repository: RepositoryName,
+    digest: Digest,
+}
+
+#[delete("/{repository:[^{}]+}/blobs/{digest}")]
+pub(crate) async fn delete(
+    app: Data<RegistryApp>,
+    req: HttpRequest,
+    path: Path<BlobRequest>,
+    token: Token,
+) -> Result<HttpResponse, RegistryError> {
+    if !token.validated_token {
+        return Err(RegistryError::MustAuthenticate {
+            challenge: token.get_push_challenge(&path.repository),
+        });
+    }
+
+    if !token.has_permission(&path.repository, "push") {
+        return Err(RegistryError::AccessDenied {});
+    }
+
+    if let Some(blob) = app.get_blob(&path.digest).await {
+        if !blob.repositories.contains(&path.repository) {
+            return Err(RegistryError::BlobNotFound {});
+        }
+    } else {
+        return Err(RegistryError::BlobNotFound {});
+    }
+
+    let actions = vec![RegistryAction::BlobUnmounted {
+        timestamp: Utc::now(),
+        digest: path.digest.clone(),
+        repository: path.repository.clone(),
+        user: token.sub.clone(),
+    }];
+
+    if !app.submit(actions).await {
+        // FIXME
+        return Err(RegistryError::BlobNotFound {});
+    }
+
+    Ok(HttpResponseBuilder::new(StatusCode::ACCEPTED).finish())
+}
--- src/network/registry/blobs/mod.rs
+++ src/network/registry/blobs/mod.rs
@@ -1,2 +1,3 @@
+pub mod delete;
 pub mod get;
 pub(crate) mod uploads;
