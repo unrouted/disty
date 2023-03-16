10a49e99df6134ef417a7dba92758c0030f78e38
John Carr <john.carr@unrouted.co.uk>
2023-03-16 10:49:00 +0000

    Use peer config when init()

--- src/network/management.rs
+++ src/network/management.rs
@@ -47,12 +47,12 @@ pub async fn change_membership(
 /// Initialize a single-node cluster.
 #[post("/init")]
 pub async fn init(app: Data<RegistryApp>) -> actix_web::Result<impl Responder> {
+    let peer = app.config.peers.get((app.id - 1) as usize).unwrap();
     let mut nodes = BTreeMap::new();
     nodes.insert(
         app.id,
         BasicNode {
-            // FIXME: Use peers config
-            addr: format!("{}:{}", &app.config.raft.address, &app.config.raft.port),
+            addr: format!("{}:{}", &peer.raft.address, &peer.raft.port),
         },
     );
     let res = app.raft.initialize(nodes).await;
