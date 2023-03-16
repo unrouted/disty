b67de342bef3ab7500fdb76f0aa25738737703f7
John Carr <john.carr@unrouted.co.uk>
2023-03-16 11:19:31 +0000

    Missing files

---
+++ src/registry/blobs/head.rs
@@ -0,0 +1,66 @@
+use crate::app::RegistryApp;
+use crate::extractors::Token;
+use crate::registry::errors::RegistryError;
+use crate::types::Digest;
+use crate::types::RepositoryName;
+use actix_web::head;
+use actix_web::http::StatusCode;
+use actix_web::web::Data;
+use actix_web::web::Path;
+use actix_web::HttpResponseBuilder;
+use actix_web::Responder;
+use serde::Deserialize;
+use tracing::debug;
+
+#[derive(Debug, Deserialize)]
+pub struct BlobRequest {
+    repository: RepositoryName,
+    digest: Digest,
+}
+
+#[head("/{repository:[^{}]+}/blobs/{digest}")]
+pub(crate) async fn head(
+    app: Data<RegistryApp>,
+    path: Path<BlobRequest>,
+    token: Token,
+) -> Result<impl Responder, RegistryError> {
+    if !token.validated_token {
+        return Err(RegistryError::MustAuthenticate {
+            challenge: token.get_pull_challenge(&path.repository),
+        });
+    }
+
+    if !token.has_permission(&path.repository, "pull") {
+        debug!("Token does not have access to perform this action");
+        return Err(RegistryError::AccessDenied {});
+    }
+
+    let blob = match app.get_blob(&path.digest).await {
+        Some(blob) => blob,
+        None => return Err(RegistryError::BlobNotFound {}),
+    };
+
+    if !blob.repositories.contains(&path.repository) {
+        tracing::debug!("Blob exists but not in repostitory: {}", path.repository);
+        return Err(RegistryError::BlobNotFound {});
+    }
+
+    let content_type = match blob.content_type {
+        Some(content_type) => content_type,
+        _ => "application/octet-steam".to_string(),
+    };
+
+    let content_length = match blob.size {
+        Some(content_length) => content_length,
+        _ => {
+            tracing::debug!("Blob was present but size not available");
+            return Err(RegistryError::BlobNotFound {});
+        }
+    };
+
+    Ok(HttpResponseBuilder::new(StatusCode::OK)
+        .content_type(content_type)
+        .append_header(("Content-Length", content_length))
+        .append_header(("Docker-Content-Digest", path.digest.to_string()))
+        .finish())
+}
---
+++ src/registry/manifests/head.rs
@@ -0,0 +1,151 @@
+use crate::app::RegistryApp;
+use crate::extractors::Token;
+use crate::registry::errors::RegistryError;
+use crate::types::Digest;
+use crate::types::RepositoryName;
+use actix_web::head;
+use actix_web::http::StatusCode;
+use actix_web::web::Data;
+use actix_web::web::Path;
+use actix_web::HttpResponse;
+use actix_web::HttpResponseBuilder;
+use serde::Deserialize;
+use tracing::debug;
+
+/*
+200 OK
+Docker-Content-Digest: <digest>
+Content-Type: <media type of manifest>
+
+{
+   "name": <name>,
+   "tag": <tag>,
+   "fsLayers": [
+      {
+         "blobSum": "<digest>"
+      },
+      ...
+    ]
+   ],
+   "history": <v1 images>,
+   "signature": <JWS>
+}
+*/
+
+#[derive(Debug, Deserialize)]
+pub struct ManifestGetRequestDigest {
+    repository: RepositoryName,
+    digest: Digest,
+}
+
+#[head("/{repository:[^{}]+}/manifests/{digest}")]
+pub(crate) async fn head(
+    app: Data<RegistryApp>,
+    path: Path<ManifestGetRequestDigest>,
+    token: Token,
+) -> Result<HttpResponse, RegistryError> {
+    if !token.validated_token {
+        return Err(RegistryError::MustAuthenticate {
+            challenge: token.get_pull_challenge(&path.repository),
+        });
+    }
+
+    if !token.has_permission(&path.repository, "pull") {
+        return Err(RegistryError::AccessDenied {});
+    }
+
+    let manifest = match app.get_manifest(&path.digest).await {
+        Some(manifest) => {
+            if !manifest.repositories.contains(&path.repository) {
+                return Err(RegistryError::ManifestNotFound {});
+            }
+            manifest
+        }
+        None => return Err(RegistryError::ManifestNotFound {}),
+    };
+
+    let content_type = match manifest.content_type {
+        Some(content_type) => content_type,
+        _ => {
+            debug!("Could not extract content type from graph");
+            return Err(RegistryError::ManifestNotFound {});
+        }
+    };
+
+    let content_length = match manifest.size {
+        Some(content_length) => content_length,
+        _ => {
+            debug!("Could not extract content length from graph");
+            return Err(RegistryError::ManifestNotFound {});
+        }
+    };
+
+    Ok(HttpResponseBuilder::new(StatusCode::OK)
+        .content_type(content_type)
+        .append_header(("Content-Length", content_length))
+        .append_header(("Docker-Content-Digest", path.digest.to_string()))
+        .finish())
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ManifestGetRequestTag {
+    repository: RepositoryName,
+    tag: String,
+}
+
+#[head("/{repository:[^{}]+}/manifests/{tag}")]
+pub(crate) async fn head_by_tag(
+    app: Data<RegistryApp>,
+    path: Path<ManifestGetRequestTag>,
+    token: Token,
+) -> Result<HttpResponse, RegistryError> {
+    if !token.validated_token {
+        return Err(RegistryError::MustAuthenticate {
+            challenge: token.get_pull_challenge(&path.repository),
+        });
+    }
+
+    if !token.has_permission(&path.repository, "pull") {
+        return Err(RegistryError::AccessDenied {});
+    }
+
+    let digest = match app.get_tag(&path.repository, &path.tag).await {
+        Some(tag) => tag,
+        None => {
+            debug!("No such tag");
+            return Err(RegistryError::ManifestNotFound {});
+        }
+    };
+
+    let manifest = match app.get_manifest(&digest).await {
+        Some(manifest) => {
+            if !manifest.repositories.contains(&path.repository) {
+                return Err(RegistryError::ManifestNotFound {});
+            }
+            manifest
+        }
+        None => return Err(RegistryError::ManifestNotFound {}),
+    };
+
+    let content_type = match manifest.content_type {
+        Some(content_type) => content_type,
+        _ => {
+            debug!("Could not extract content type from graph");
+            return Err(RegistryError::ManifestNotFound {});
+        }
+    };
+
+    let content_length = match manifest.size {
+        Some(content_length) => content_length,
+        _ => {
+            debug!("Could not extract content length from graph");
+            return Err(RegistryError::ManifestNotFound {});
+        }
+    };
+
+    Ok(HttpResponseBuilder::new(StatusCode::OK)
+        .content_type(content_type)
+        .append_header(("Content-Length", content_length))
+        .append_header(("Docker-Content-Digest", digest.to_string()))
+        .finish())
+}
