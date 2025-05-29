5fe5b02b91ba6f370a6e9971106948395757e51b
John Carr <john.carr@unrouted.co.uk>
2025-05-29 15:58:36 +0100

    wip: negative assertions for acl test

--- src/registry/token.rs
+++ src/registry/token.rs
@@ -197,7 +197,165 @@ mod test {
         let body = res.into_body().collect().await.unwrap().to_bytes();
         let value: TokenResponse = serde_json::from_slice(&body)?;
 
-        println!("{:?}", value);
+        let issuer = fixture.state.config.issuer.as_ref().unwrap();
+
+        let options = VerificationOptions {
+            // accept tokens even if they have expired up to 15 minutes after the deadline
+            time_tolerance: Some(Duration::from_mins(15)),
+            // reject tokens if they were issued more than 1 hour ago
+            max_validity: Some(Duration::from_hours(1)),
+            // reject tokens if they don't include an issuer from that list
+            allowed_issuers: Some(HashSet::from_strings(&[issuer.issuer.clone()])),
+            // validate it is a token for us
+            allowed_audiences: Some(HashSet::from_strings(&[issuer.audience.clone()])),
+            ..Default::default()
+        };
+
+        let claims: JWTClaims<Value> = issuer
+            .key_pair
+            .key_pair
+            .public_key()
+            .verify_token(&value.token, Some(options))?;
+
+        assert_json_eq!(claims.custom, json!({"access": []}));
+
+        fixture.teardown().await
+    }
+
+    #[test(tokio::test)]
+    pub async fn user_authentication_and_authorization_with_matching_acl() -> Result<()> {
+        let fixture = RegistryFixture::with_state(
+            FixtureBuilder::new()
+                .issuer(true)
+                .user(User::Password {
+                    username: "username".into(),
+                    password: "$6$TVFDl34in89H8PM.$vJ2jhuC0Ijgr9c5.uijvYp31g0K4x2jl6FDpdfw40CVdFjzyO7pJpGLkVIAGtwsbbS1RcWgJ0VNSR83Uf.T..1".into(),
+                })
+                .acl(AccessRule {
+                    check: MatchRule {
+                        username: Some(StringMatch::Exact("username".into())),
+                        ..Default::default()
+                    },
+                    actions: [Action::Pull].into_iter().collect(),
+                    comment: "username can pull this image".into(),
+                })
+                .build()
+                .await?,
+        )?;
+
+        let credentials = format!("{}:{}", "username", "password");
+        let encoded = STANDARD.encode(credentials);
+        let value = format!("Basic {}", encoded);
+
+        let res = fixture
+            .request(
+                Request::builder()
+                    .extension(ConnectInfo::<SocketAddr>("127.0.0.1:8123".parse()?))
+                    .method("GET")
+                    .uri("/token?service=foo&scope=repository%3Abadger%3Apull%2Cpush")
+                    .header("Authorization", value)
+                    .body(Body::empty())?,
+            )
+            .await?;
+
+        assert_eq!(res.status(), StatusCode::OK);
+
+        let body = res.into_body().collect().await.unwrap().to_bytes();
+        let value: TokenResponse = serde_json::from_slice(&body)?;
+
+        let issuer = fixture.state.config.issuer.as_ref().unwrap();
+
+        let options = VerificationOptions {
+            // accept tokens even if they have expired up to 15 minutes after the deadline
+            time_tolerance: Some(Duration::from_mins(15)),
+            // reject tokens if they were issued more than 1 hour ago
+            max_validity: Some(Duration::from_hours(1)),
+            // reject tokens if they don't include an issuer from that list
+            allowed_issuers: Some(HashSet::from_strings(&[issuer.issuer.clone()])),
+            // validate it is a token for us
+            allowed_audiences: Some(HashSet::from_strings(&[issuer.audience.clone()])),
+            ..Default::default()
+        };
+
+        let claims: JWTClaims<Value> = issuer
+            .key_pair
+            .key_pair
+            .public_key()
+            .verify_token(&value.token, Some(options))?;
+
+        assert_json_eq!(
+            claims.custom,
+            json!({"access": [{
+                "name": "badger",
+                "type": "repository",
+                "actions": ["pull"],
+            }]})
+        );
+
+        fixture.teardown().await
+    }
+    #[test(tokio::test)]
+    pub async fn user_authentication_and_authorization_with_non_matching_acl() -> Result<()> {
+        let fixture = RegistryFixture::with_state(
+            FixtureBuilder::new()
+                .issuer(true)
+                .user(User::Password {
+                    username: "username".into(),
+                    password: "$6$TVFDl34in89H8PM.$vJ2jhuC0Ijgr9c5.uijvYp31g0K4x2jl6FDpdfw40CVdFjzyO7pJpGLkVIAGtwsbbS1RcWgJ0VNSR83Uf.T..1".into(),
+                })
+                .acl(AccessRule {
+                    check: MatchRule {
+                        username: Some(StringMatch::Exact("bob".into())),
+                        ..Default::default()
+                    },
+                    actions: [Action::Pull].into_iter().collect(),
+                    comment: "username can pull this image".into(),
+                })
+                .build()
+                .await?,
+        )?;
+
+        let credentials = format!("{}:{}", "username", "password");
+        let encoded = STANDARD.encode(credentials);
+        let value = format!("Basic {}", encoded);
+
+        let res = fixture
+            .request(
+                Request::builder()
+                    .extension(ConnectInfo::<SocketAddr>("127.0.0.1:8123".parse()?))
+                    .method("GET")
+                    .uri("/token?service=foo&scope=repository%3Abadger%3Apull%2Cpush")
+                    .header("Authorization", value)
+                    .body(Body::empty())?,
+            )
+            .await?;
+
+        assert_eq!(res.status(), StatusCode::OK);
+
+        let body = res.into_body().collect().await.unwrap().to_bytes();
+        let value: TokenResponse = serde_json::from_slice(&body)?;
+
+        let issuer = fixture.state.config.issuer.as_ref().unwrap();
+
+        let options = VerificationOptions {
+            // accept tokens even if they have expired up to 15 minutes after the deadline
+            time_tolerance: Some(Duration::from_mins(15)),
+            // reject tokens if they were issued more than 1 hour ago
+            max_validity: Some(Duration::from_hours(1)),
+            // reject tokens if they don't include an issuer from that list
+            allowed_issuers: Some(HashSet::from_strings(&[issuer.issuer.clone()])),
+            // validate it is a token for us
+            allowed_audiences: Some(HashSet::from_strings(&[issuer.audience.clone()])),
+            ..Default::default()
+        };
+
+        let claims: JWTClaims<Value> = issuer
+            .key_pair
+            .key_pair
+            .public_key()
+            .verify_token(&value.token, Some(options))?;
+
+        assert_json_eq!(claims.custom, json!({"access": []}));
 
         fixture.teardown().await
     }
@@ -380,6 +538,102 @@ mod test {
                 "actions": ["pull"],
             }]})
         );
+
+        fixture.teardown().await
+    }
+
+    #[test(tokio::test)]
+    pub async fn user_authentication_and_authorization_with_idtoken_non_matching_acl() -> Result<()>
+    {
+        // Build an IDToken a bit like what GitLab generates
+        let key: RS256KeyPair = RS256KeyPair::generate(2048)?.with_key_id("bob");
+        let custom_claims = [
+            (
+                "project_path".to_string(),
+                "my-group/my-project".to_string(),
+            ),
+            ("ref".to_string(), "feature-branch-1".to_string()),
+        ]
+        .into_iter()
+        .collect::<HashMap<String, String>>();
+        let claims = Claims::with_custom_claims(custom_claims, Duration::from_mins(10))
+            .with_issuer("gitlab.example.com")
+            .with_audience("localhost")
+            .with_subject("project_path:my-group/my-project:ref_type:branch:ref:feature-branch-1");
+
+        let token = key.sign(claims)?;
+
+        let issuer = JWKSPublicKey::new("http://localhost", "gitlab.example.com", "localhost")
+            .with_cache(key.public_key())
+            .await;
+
+        let fixture = RegistryFixture::with_state(
+            FixtureBuilder::new()
+                .issuer(true)
+                .user(User::Token {
+                    username: "gitlab".into(),
+                    issuer,
+                })
+                .acl(AccessRule {
+                    check: MatchRule {
+                        claims: Some(
+                            [(
+                                "project_path".to_string(),
+                                StringMatch::Exact("my-group/bobs-project".into()),
+                            )]
+                            .into_iter()
+                            .collect(),
+                        ),
+                        ..Default::default()
+                    },
+                    actions: [Action::Pull].into_iter().collect(),
+                    comment: "Gitlab can pull this image".into(),
+                })
+                .build()
+                .await?,
+        )?;
+
+        let credentials = format!("{}:{}", "gitlab", token);
+        let encoded = STANDARD.encode(credentials);
+        let value = format!("Basic {}", encoded);
+
+        let res = fixture
+            .request(
+                Request::builder()
+                    .extension(ConnectInfo::<SocketAddr>("127.0.0.1:8123".parse()?))
+                    .method("GET")
+                    .uri("/token?service=foo&scope=repository%3Abadger%3Apull%2Cpush")
+                    .header("Authorization", value)
+                    .body(Body::empty())?,
+            )
+            .await?;
+
+        assert_eq!(res.status(), StatusCode::OK);
+
+        let body = res.into_body().collect().await.unwrap().to_bytes();
+        let value: TokenResponse = serde_json::from_slice(&body)?;
+
+        let issuer = fixture.state.config.issuer.as_ref().unwrap();
+
+        let options = VerificationOptions {
+            // accept tokens even if they have expired up to 15 minutes after the deadline
+            time_tolerance: Some(Duration::from_mins(15)),
+            // reject tokens if they were issued more than 1 hour ago
+            max_validity: Some(Duration::from_hours(1)),
+            // reject tokens if they don't include an issuer from that list
+            allowed_issuers: Some(HashSet::from_strings(&[issuer.issuer.clone()])),
+            // validate it is a token for us
+            allowed_audiences: Some(HashSet::from_strings(&[issuer.audience.clone()])),
+            ..Default::default()
+        };
+
+        let claims: JWTClaims<Value> = issuer
+            .key_pair
+            .key_pair
+            .public_key()
+            .verify_token(&value.token, Some(options))?;
+
+        assert_json_eq!(claims.custom, json!({"access": []}));
 
         fixture.teardown().await
     }
