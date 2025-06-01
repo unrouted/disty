d4e53aa42c8a6667d02720d8633f4eda69668336
John Carr <john.carr@unrouted.co.uk>
2025-06-01 22:15:08 +0100

    fix: middleware was stripping query string

--- src/registry/middleware.rs
+++ src/registry/middleware.rs
@@ -35,7 +35,9 @@ where
     }
 
     fn call(&mut self, mut req: Request<ReqBody>) -> Self::Future {
-        let path = req.uri().path().to_string();
+        let uri = req.uri();
+        let path = uri.path().to_string();
+        let query = uri.query().unwrap_or("");
 
         let re = Regex::new(r"(^/v2/)(.+)(/(manifests|blobs|tags).*$)").unwrap();
 
@@ -51,9 +53,10 @@ where
 
         println!("{}", result);
 
-        let mut parts = req.uri().clone().into_parts();
-        parts.path_and_query = Some(result.parse().unwrap());
-        let new_uri = Uri::from_parts(parts).unwrap();
+        let new_uri = format!("{}?{}", result, query)
+            .parse::<Uri>()
+            .expect("Failed to build URI");
+
         *req.uri_mut() = new_uri;
 
         println!("{}", req.uri().path());
@@ -67,7 +70,13 @@ mod tests {
     use crate::error::RegistryError;
 
     use super::*;
-    use axum::{Router, body::Body, extract::Path, http::Request, response::Response};
+    use axum::{
+        Router,
+        body::Body,
+        extract::{Path, Query},
+        http::Request,
+        response::Response,
+    };
     use http_body_util::BodyExt;
     use reqwest::StatusCode;
     use serde::Deserialize;
@@ -79,10 +88,16 @@ mod tests {
         tag: String,
     }
 
+    #[derive(Debug, Deserialize)]
+    pub struct QueryRequest {
+        bob: Option<String>,
+    }
+
     pub(crate) async fn echo_path(
         Path(ManifestGetRequest { repository, tag }): Path<ManifestGetRequest>,
+        Query(QueryRequest { bob }): Query<QueryRequest>,
     ) -> Result<Response, RegistryError> {
-        let resp = format!("{repository}:{tag}");
+        let resp = format!("{repository}:{tag}\n{bob:?}");
         Ok(Response::builder()
             .status(StatusCode::OK)
             .body(Body::from(resp))?)
@@ -109,6 +124,30 @@ mod tests {
 
         let body = res.into_body().collect().await.unwrap().to_bytes();
         println!("{:?}", body);
-        assert_eq!(&body[..], b"some/repo:latest");
+        assert_eq!(&body[..], b"some/repo:latest\nNone");
+    }
+
+    #[tokio::test]
+    async fn test_uri_rewriting_with_query() {
+        let app = Router::new().route(
+            "/v2/{repository}/manifests/{tag}",
+            axum::routing::get(echo_path),
+        );
+
+        let app = tower::ServiceBuilder::new()
+            .layer(RewriteUriLayer)
+            .service(app);
+
+        let req = Request::builder()
+            .uri("/v2/some/repo/manifests/latest?bob=uncertain")
+            .body(Body::empty())
+            .unwrap();
+
+        let res = app.oneshot(req).await.unwrap();
+        assert_eq!(res.status(), StatusCode::OK);
+
+        let body = res.into_body().collect().await.unwrap().to_bytes();
+        println!("{:?}", body);
+        assert_eq!(&body[..], b"some/repo:latest\nSome(\"uncertain\")");
     }
 }
